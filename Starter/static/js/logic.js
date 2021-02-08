5dbfce40d9b7903215fccf4a1d87734d27d3bc24
Sherin M <sherinm@Sherins-MacBook-Pro.local>
2021-02-08 00:01:25 -0500

    updates

--- Starter/static/js/logic.js
+++ Starter/static/js/logic.js
@@ -4,7 +4,7 @@
 var myMap = L.map("map", {
     center: [
      38.09, -95.71],
-    zoom: 9
+    zoom: 2
   });
 
  //layer street 
@@ -17,17 +17,15 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: API_KEY
 }).addTo(myMap);
 
-
+ // all earthquake from past seven days
   // url for earthquak, week-
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
   
-  // making marker for mag values, scaling properly 
+  //  mag values, scaling properly 
   function markerSize(mag){
     return mag * 2
-  }
-  // colors function 
-   // all earthquake from past seven days
-
+  } 
+  
 // magnitude is proportial to size, as mag increases, size increases
 
   // adding colors - as earthequak depth increases, colors gets darker, going from light blue to eventually dark red
@@ -37,11 +35,34 @@ var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.ge
   // darker organge tint #ffa329
   // darkest orange tint #ed8700
   // red #c60f00
+
+function chooseColor(d) {
+    switch (true) {
+    case (d > 85):
+      return "#c60f00";
+    case (d > 75 ):
+      return "#ed8700" ;
+    case (d > 65):
+        return "#ffa329";
+    case (d > 55):
+          return "#ffc577";
+    case (d> 35):
+            return "#90eff7"; 
+    case (d < 15):
+              return "#b5f4fa"; 
+    //default:
+       //  return "#b5f4fa";
+    }
+  }
   
+
   
   
    //Get GeoJSON data..
 d3.json(url, function(myData) {
-    console.log(myData)
+    //console.log(myData)
    
 });
+
+
+
