8282924b6c194019e89d25b710c21e7d1df9d627
Sherin M <sherinm@Sherins-MacBook-Pro.local>
2021-02-07 19:44:04 -0500

    updates

--- Starter/static/js/logic.js
+++ Starter/static/js/logic.js
@@ -26,6 +26,18 @@ var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.ge
     return mag * 2
   }
   // colors function 
+   // all earthquake from past seven days
+
+// magnitude is proportial to size, as mag increases, size increases
+
+  // adding colors - as earthequak depth increases, colors gets darker, going from light blue to eventually dark red
+  // light blue #b5f4fa
+  // a darker blue tint #90eff7
+  // light orange #ffc577
+  // darker organge tint #ffa329
+  // darkest orange tint #ed8700
+  // red #c60f00
+  
   
   
    //Get GeoJSON data..
